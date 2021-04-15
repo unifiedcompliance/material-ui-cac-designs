b571a71586a3cf1816ccfbaec1a930a2b45d6f3a
aaron <aaron.louis228@gmail.com>
2021-04-15 11:21:24 -0400

    Remove chartjs dependency

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import 'react-perfect-scrollbar/dist/css/styles.css';
 import { useRoutes } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
 import GlobalStyles from 'src/components/GlobalStyles';
-import 'src/mixins/chartjs';
 import theme from 'src/theme';
 import routes from 'src/routes';
 
